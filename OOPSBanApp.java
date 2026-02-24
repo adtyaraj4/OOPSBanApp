9e084edf0fdd497661092212020308ecb0efc7fb
adtyaraj4 <adtyaraj4@gmail.com>
2026-02-24 15:43:16 +0530

    UC6 Done

--- OOPSBanApp.java
+++ OOPSBanApp.java
@@ -1,21 +1,56 @@
 public class OOPSBanApp {
 
+    // Method to generate O pattern
+    public static String[] getOPattern() {
+        return new String[]{
+                " ***  ",
+                "*   * ",
+                "*   * ",
+                "*   * ",
+                "*   * ",
+                "*   * ",
+                " ***  "
+        };
+    }
+
+    // Method to generate P pattern
+    public static String[] getPPattern() {
+        return new String[]{
+                "***** ",
+                "*    *",
+                "*    *",
+                "***** ",
+                "*     ",
+                "*     ",
+                "*     "
+        };
+    }
+
+    // Method to generate S pattern
+    public static String[] getSPattern() {
+        return new String[]{
+                " **** ",
+                "*     ",
+                "*     ",
+                " ***  ",
+                "    * ",
+                "    * ",
+                "****  "
+        };
+    }
+
     public static void main(String[] args) {
 
-        // Inline declaration + initialization
-        String[] lines = {
-                String.join(" ", " ***  ", "  ", " ***  ", "  ", "***** ", "  ", " **** "),
-                String.join(" ", "*   * ", "  ", "*   * ", "  ", "*    *", "  ", "*     "),
-                String.join(" ", "*   * ", "  ", "*   * ", "  ", "*    *", "  ", "*     "),
-                String.join(" ", "*   * ", "  ", "*   * ", "  ", "***** ", "  ", " ***  "),
-                String.join(" ", "*   * ", "  ", "*   * ", "  ", "*     ", "  ", "    * "),
-                String.join(" ", "*   * ", "  ", "*   * ", "  ", "*     ", "  ", "    * "),
-                String.join(" ", " ***  ", "  ", " ***  ", "  ", "*     ", "  ", "****  ")
-        };
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-        // Print using loop
-        for (String line : lines) {
-            System.out.println(line);
+        // Loop to assemble O O P S
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(oPattern[i] + "  "
+                    + oPattern[i] + "  "
+                    + pPattern[i] + "  "
+                    + sPattern[i]);
         }
     }
 }
